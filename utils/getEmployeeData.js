e6b9d189fa782050f090ac2ebbe5816669db9a84
Dolo_Mane <zimmerman.lucas@hotmail.com>
2022-04-12 12:13:09 -0500

    Update to getEmployeeData();

--- utils/getEmployeeData.js
+++ utils/getEmployeeData.js
@@ -9,13 +9,23 @@ const employeeArray = [];
 
 // Function to get all employees
 const getEmployeeData = (data) => {
-    // pulling manager data
+    // Pulling manager data
     const manager = new Manager(data.name, data.employeeId, data.email, data.officeNum);
     employeeArray.push(manager);
+
+    // Pulling employee data
+    for(let i = 0; i < data.employees.length; i++) {
+        if(data.employees[i].role === "Engineer"){
+            const engineer = new Engineer(data.employees[i].name, data.employees[i].employeeId, data.employees[i].email, data.employees[i].github);
+            employeeArray.push(engineer);
+        }
+        else{
+            const intern = new Intern(data.employees[i].name, data.employees[i].employeeId, data.employees[i].email, data.employees[i].school);
+            employeeArray.push(intern);
+        }
+    }
     console.log(employeeArray);
 }
-
-// TODO: Iterate through employees, pull employee data, pass data through class based on role, push to empty array
 
 // TODO: Interate through employee array, create HTML cards for each employee
 
