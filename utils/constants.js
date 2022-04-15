5a0d180cf621d00c3bbb0fa2055bb20de5012338
Dolo_Mane <zimmerman.lucas@hotmail.com>
2022-04-14 21:50:59 -0500

    Update some CSS. Fix html formatting issues on file write

--- utils/constants.js
+++ utils/constants.js
@@ -1,21 +1,5 @@
 const indexHTML = {
-    INDEX_BASE: `<!DOCTYPE html>
-    <html lang="en">
-      <head>
-        <meta charset="UTF-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css" />
-        <link rel="stylesheet" href="./dist/style.css" />
-        <title>Super Team Table Generator</title>
-      </head>
-      <body>
-        <header class="blue lighten-4">
-        Super Team Table Generator
-        </header>
-        <div class="container">
-            <div class="row" id="card-holder">`,
-
+    INDEX_BASE: `<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8" /><meta http-equiv="X-UA-Compatible" content="IE=edge" /><meta name="viewport" content="width=device-width, initial-scale=1.0" /><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css"/><link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"/><link rel="stylesheet" href="./style.css"/><title>Super Team Table Generator</title></head><body><header class="blue lighten-4">Super Team Table Generator</header><div class="container"><div class="row" id="card-holder">`,
     INDEX_END: `</div>
     </div>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
@@ -24,3 +8,6 @@ const indexHTML = {
 };
 
 module.exports = indexHTML;
+
+
+
--- utils/getEmployeeData.js
+++ utils/getEmployeeData.js
@@ -5,13 +5,12 @@ const Manager = require('../lib/Manager');
 const Constants = require('./constants');
 const fs = require = require('fs');
 
-
-// Variables
-const employeeArray = [];
-
 // Function to get all employees
 const getEmployeeData = (data) => {
-    // Pulling manager data
+
+	const employeeArray = [];
+    
+	// Pulling manager data
     const manager = new Manager(data.name, data.employeeId, data.email, data.officeNum);
     employeeArray.push(manager);
 
@@ -26,54 +25,56 @@ const getEmployeeData = (data) => {
             employeeArray.push(intern);
         }
     }
-	// console.log(employeeArray);
-	createEmployeeCards(employeeArray);
+	
+	createHtml(employeeArray);
 }
 
-// TODO: Interate through employee array, create HTML cards for each employee
-
-//TODO: Take index.html as a file var, update with cards, save as a copy in root
-const createEmployeeCards = (teamMemebers) => {
+// Function that takes employees data with literal templates and creates an html file
+const createHtml = (teamMemebers) => {
 	const cardList = [];
+
+	// Create cards based on employee role
 	for (let i = 0; i < teamMemebers.length; i++) {
 		if (teamMemebers[i].getRole() === 'Manager') {
 			cardList.push(`<div class="col l3 m6 s12">
-			<div class="card">
+			<div class="card z-depth-5">
 			  <div class="card-content blue white-text">
-			  <span class="row employee-name">${teamMemebers[i].getName()}</span>
-			  <span class="row employee-role">${teamMemebers[i].getRole()}</span>
+			  <span class="row employee-header">${teamMemebers[i].getName()}</span>
+			  <i class="material-icons">free_breakfast</i>
+			  <span class="row employee-header"> ${teamMemebers[i].getRole()}</span>
 				</div>
 			<div class="card-action blue lighten-4">
 			  <div class="row detail-rows">
-			  <span id="employee-id">${teamMemebers[i].getId()}</span>
+			  <span id="employee-id">ID: ${teamMemebers[i].getId()}</span>
 			</div>
 			<div class="row detail-rows">
-			  <span>Email:</span>
+			  <span>Email: </span>
 			  <a href="mailto:${teamMemebers[i].getEmail()}" id="employee-email">${teamMemebers[i].getEmail()}</a>
 			</div>
 			<div class="row detail-rows">
-			  <span id="unique-role-value">${teamMemebers[i].getOfficeNumber()}</span>
+			  <span id="unique-role-value">Office Number: ${teamMemebers[i].getOfficeNumber()}</span>
 			</div>
 			</div>
 			</div>
 		  </div>`)
 		} else if (teamMemebers[i].getRole() === 'Engineer') {
 			cardList.push(`<div class="col l3 m6 s12">
-			<div class="card">
+			<div class="card z-depth-5">
 			  <div class="card-content blue white-text">
-			  <span class="row employee-name">${teamMemebers[i].getName()}</span>
-			  <span class="row employee-role">${teamMemebers[i].getRole()}</span>
+			  <span class="row employee-header">${teamMemebers[i].getName()}</span>
+			  <i class="material-icons">build</i>
+			  <span class="row employee-header"> ${teamMemebers[i].getRole()}</span>
 			</div>
 			<div class="card-action blue lighten-4">
 			  <div class="row detail-rows">
-			  <span id="employee-id">${teamMemebers[i].getId()}</span>
+			  <span id="employee-id">ID: ${teamMemebers[i].getId()}</span>
 			</div>
 			<div class="row detail-rows">
-			  <span></span>
+			  <span>Email: </span>
 			  <a href="mailto:${teamMemebers[i].getEmail()}" id="employee-email">${teamMemebers[i].getEmail()}</a>
 			</div>
 			<div class="row detail-rows">
-			  <span id="unique-role-value">${teamMemebers[i].getGithub()}</span>
+			  <span id="unique-role-value">GitHub: ${teamMemebers[i].getGithub()}</span>
 			</div>
 			</div>
 			</div>
@@ -81,59 +82,63 @@ const createEmployeeCards = (teamMemebers) => {
 		} else {
 			cardList.push(
 				`<div class="col l3 m6 s12">
-			<div class="card">
+			<div class="card z-depth-5">
 			  <div class="card-content blue white-text">
-			  <span class="row employee-name">${teamMemebers[i].getName()}</span>
-			  <span class="row employee-role">${teamMemebers[i].getRole()}</span>
+			  <span class="row employee-header">${teamMemebers[i].getName()}</span>
+			  <i class="material-icons">school</i>
+			  <span class="row employee-header"> ${teamMemebers[i].getRole()}</span>
 			</div>
 			<div class="card-action blue lighten-4">
 			  <div class="row detail-rows">
-			  <span id="employee-id">${teamMemebers[i].getId()}</span>
+			  <span id="employee-id">ID: ${teamMemebers[i].getId()}</span>
 			</div>
 			<div class="row detail-rows">
-			  <span></span>
+			  <span>Email: </span>
 			  <a href="mailto:${teamMemebers[i].getEmail()}" id="employee-email">${teamMemebers[i].getEmail()}</a>
 			</div>
 			<div class="row detail-rows">
-			  <span id="unique-role-value">${teamMemebers[i].getSchool()}</span>
+			  <span id="unique-role-value">School: ${teamMemebers[i].getSchool()}</span>
 			</div>
 			</div>
 			</div>
 		  </div>`)
 		}
-		// console.log(cardList);
 	};
+
+	// Combine html constant templates with cards created
 	let indexVal = Constants.INDEX_BASE + cardList.join('') + Constants.INDEX_END;
-	console.log(indexVal);
+	indexVal = format(indexVal);
+	
+	// Write results to file
 	fs.writeFile('./dist/index.html', indexVal, err => {
 		if (err) {
 		  console.error(err)
 		  return
 		}
 		console.log('file written successfully');
-	  })	  
+	  })
 };
 
-`<div class="col l3 m6 s12">
-    <div class="card">
-      <div class="card-content blue white-text">
-      <span class="row employee-name"></span>
-      <span class="row employee-role"></span>
-    </div>
-    <div class="card-action blue lighten-4">
-      <div class="row detail-rows">
-      <span id="employee-id"></span>
-    </div>
-    <div class="row detail-rows">
-      <span></span>
-      <a href="mailto: insertemailhere@xyz.com?subject=Mail from xyz.com" id="employee-email"></a>
-    </div>
-    <div class="row detail-rows">
-      <span id="unique-role-value"></span>
-    </div>
-  </div>`
+// Funtion to format html
+function format(html) {
+    var space = '  ';
+    var result = '';
+    var indent= '';
 
-// TODO: write to HTML
+    html.split(/>\s*</).forEach(function(element) {
+        if (element.match( /^\/\w/ )) {
+            indent = indent.substring(space.length);
+        }
+
+        result += indent + '<' + element + '>\r\n';
+
+        if (element.match( /^<?\w[^>]*[^\/]$/ )) { 
+            indent += space;              
+        }
+    });
+
+    return result.substring(1, result.length-3);
+}
 
 // Exporting file
 module.exports = getEmployeeData;
